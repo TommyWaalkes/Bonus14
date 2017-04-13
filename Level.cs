d960c9b90e71b9d17912abb242076c77054c0511
TommyWaalkes <Tommy@grandcircus.co>
2017-04-13 13:59:17 -0400

    Add files via upload

---
+++ Level.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus14
+{
+    class Level
+    {
+        private Space[] spaces = new Space[10];
+    }
+}
---
+++ ParkingGarage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus14
+{
+    class ParkingGarage
+    {
+        List<Level> levels = new List<Level>();
+
+        public Level GetLevel(int index)
+        {
+            Level l = levels[index];
+            return l;
+        }
+    }
+}
